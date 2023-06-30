59968f16fbfcc16c2f597d2cf501b8a459538cac
Jared Jaramillo <jared.jaramillo117@gmail.com>
2023-06-30 12:41:35 -0400

    Update Chapter_4_Notes.js
    Initial notes on Chapter 4. Simple stuff I already new.

--- Chapter_4_Notes.js
+++ Chapter_4_Notes.js
@@ -2,3 +2,16 @@
 
 //----------------------------------Chapter 4: Data Structures - Objects and Arrays----------------------------------//
 
+//Basic Intro to arrays
+let listOfNumbers = [2,3,5,7,11];
+console.log(listOfNumbers);
+
+//'.push' adds values to the end of a list
+//'.pop' removes the last value in an array and returns it. 
+//'in' is a binary operator used to determine if an object has a given property within it
+let anObject = {left:1, right:2};
+console.log("left" in anObject);
+//-> true
+
+//'object.keys' can be used to determine what proerties are in an object
+
